5e439e2d1f2215fa7eae641729bba99c5c917606
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2022-07-09 15:50:39 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -34,7 +34,7 @@
             </div>
         </div>
     </div>
-    <script src="assets/search.js"></script>
+    <script src="https://jiotvvercel11.vercel.app/api/assets/search.js"></script>
 </body>
 
 </html>
